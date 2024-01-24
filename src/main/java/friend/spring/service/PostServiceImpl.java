f221c087700e4cb69cc21bce52b55600ff37fc08
hjh <ghkdwngns@naver.com>
2024-01-24 12:40:49 +0900

    PostService

---
+++ src/main/java/friend/spring/service/PostServiceImpl.java
@@ -0,0 +1,48 @@
+package friend.spring.service;
+
+import friend.spring.converter.General_voteConverter;
+import friend.spring.converter.PostConverter;
+import friend.spring.domain.General_vote;
+import friend.spring.domain.Post;
+import friend.spring.domain.User;
+import friend.spring.domain.enums.PostType;
+import friend.spring.repository.General_voteRepository;
+import friend.spring.repository.PostRepository;
+import friend.spring.repository.UserRepository;
+import friend.spring.web.dto.PostRequestDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static friend.spring.domain.enums.PostType.VOTE;
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService{
+
+    private final PostRepository postRepository;
+    private final General_voteRepository generalVoteRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public Post joinPost(PostRequestDTO.AddPostDTO request, Long userId) {
+        Post newPost= PostConverter.toPost(request);
+        User user=userRepository.findById(userId)
+                .orElseThrow(()->new RuntimeException("\""+userId+"\"해당 유저가 없습니다"));
+        newPost.setUser(user);
+
+        if(newPost.getPostType()==VOTE&&request.getPollOption()!=null){
+            General_vote generalVote = General_voteConverter.toGeneralVoteList(request.getPollOption());
+            newPost.setGeneralVote(generalVote);
+
+            generalVoteRepository.save(generalVote);
+
+        }
+
+        return postRepository.save(newPost);
+    }
+}
