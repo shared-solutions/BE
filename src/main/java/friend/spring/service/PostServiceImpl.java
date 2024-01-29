364c73b33d9422986fb4a5644bfac92f33d66238
hjh <ghkdwngns@naver.com>
2024-01-29 23:40:25 +0900

    변경) 1. 글 종류에 맞게 설정(게이지투표 미완성)

--- src/main/java/friend/spring/service/PostServiceImpl.java
+++ src/main/java/friend/spring/service/PostServiceImpl.java
@@ -1,28 +1,33 @@
 package friend.spring.service;
 
-import friend.spring.converter.General_voteConverter;
 import friend.spring.converter.PostConverter;
-import friend.spring.domain.General_vote;
+import friend.spring.domain.Candidate;
+import friend.spring.domain.General_poll;
 import friend.spring.domain.Post;
 import friend.spring.domain.User;
-import friend.spring.repository.General_voteRepository;
+import friend.spring.domain.enums.PostVoteType;
+import friend.spring.repository.CandidateRepository;
+import friend.spring.repository.General_PollRepository;
 import friend.spring.repository.PostRepository;
 import friend.spring.repository.UserRepository;
+import friend.spring.web.dto.PollOptionDTO;
 import friend.spring.web.dto.PostRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
-import static friend.spring.domain.enums.PostType.VOTE;
+import static friend.spring.domain.enums.PostType.*;
+import static friend.spring.domain.enums.PostVoteType.GAUGE;
+import static friend.spring.domain.enums.PostVoteType.GENERAL;
 
 @Service
 @RequiredArgsConstructor
 public class PostServiceImpl implements PostService{
 
     private final PostRepository postRepository;
-    private final General_voteRepository generalVoteRepository;
+    private final General_PollRepository generalPollRepository;
     private final UserRepository userRepository;
+    private final CandidateRepository candidateRepository;
 
     @Override
     @Transactional
@@ -31,13 +36,54 @@ public class PostServiceImpl implements PostService{
         User user=userRepository.findById(userId)
                 .orElseThrow(()->new RuntimeException("\""+userId+"\"해당 유저가 없습니다"));
         newPost.setUser(user);
+        if(request.getTag()!=null){
+            newPost.setTags(request.getTag());
+        }
 
-        if(newPost.getPostType()==VOTE&&request.getPollOption()!=null){
-            General_vote generalVote = General_voteConverter.toGeneralVoteList(request.getPollOption());
-            newPost.setGeneralVote(generalVote);
+//        if(newPost.getPostType()==NOT_VOTE){
+//
+//        }
 
-            generalVoteRepository.save(generalVote);
+        if(newPost.getPostType()==VOTE&&newPost.getVoteType()==GENERAL&&request.getPollOption()!=null){
+            General_poll generalPoll = General_poll.builder()
+                    .deadline(request.getDeadline())
+                    .build();
+            newPost.setGeneralPoll(generalPoll);
+            generalPollRepository.save(generalPoll);
 
+            for (PollOptionDTO option : request.getPollOption()) {
+                Candidate candidate = Candidate.builder()
+                        .name(option.getOptionString())
+                        .image(option.getOptionImg())
+                        .build();
+
+                candidate.setGeneralPoll(generalPoll);
+                candidateRepository.save(candidate);
+            }
+        }
+
+        if(newPost.getPostType()==VOTE&&newPost.getVoteType()==GAUGE&&request.getPollOption()!=null){
+            General_poll generalPoll = General_poll.builder()
+                    .deadline(request.getDeadline())
+                    .build();
+            newPost.setGeneralPoll(generalPoll);
+            generalPollRepository.save(generalPoll);
+
+            for (PollOptionDTO option : request.getPollOption()) {
+                Candidate candidate = Candidate.builder()
+                        .name(option.getOptionString())
+                        .image(option.getOptionImg())
+                        .build();
+
+                candidate.setGeneralPoll(generalPoll);
+                candidateRepository.save(candidate);
+            }
+        }
+
+        if(newPost.getPostType()==REVIEW&&request.getParent_id()!=null){
+            Post parent=postRepository.findById(request.getParent_id())
+                            .orElseThrow(()->new RuntimeException("\""+request.getParent_id()+"\"해당 글이 없습니다"));
+            newPost.setParentPost(parent);
         }
 
         return postRepository.save(newPost);
