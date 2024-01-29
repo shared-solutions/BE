ed9898c95bc7625167d632c2f0e78777f195800b
hjh <ghkdwngns@naver.com>
2024-01-29 23:37:22 +0900

    변경) 삭제

--- src/main/java/friend/spring/repository/General_voteRepository.java
+++
@@ -1,7 +0,0 @@
-package friend.spring.repository;
-
-import friend.spring.domain.General_vote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface General_voteRepository extends JpaRepository<General_vote, Long> {
-}
