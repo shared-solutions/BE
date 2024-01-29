988d33c3d38de11f6de5f5de2afb78153ef790c7
hjh <ghkdwngns@naver.com>
2024-01-29 23:39:48 +0900

    변경) 1. 부모 id LONG타입으로 입력      2. file삭제      3. tag list로 입력

--- src/main/java/friend/spring/web/dto/PostRequestDTO.java
+++ src/main/java/friend/spring/web/dto/PostRequestDTO.java
@@ -1,7 +1,10 @@
 package friend.spring.web.dto;
 
 
+import friend.spring.domain.Post;
 import lombok.Getter;
+import org.apache.tomcat.jni.Poll;
+
 import javax.validation.constraints.NotBlank;
 import java.sql.Timestamp;
 import java.util.List;
@@ -17,11 +20,12 @@ public class PostRequestDTO {
         Integer postType; // 1: not vote, 2: vote, 3: review
         Integer category; //미정
         Integer postVoteType;// 1: general, 2: gauge
-//        Integer parent_id;
-        List<String> pollOption;
-        String file;
-        String tag;
+        Long parent_id;
+        List<PollOptionDTO> pollOption;
+        List<String> tag;
         Timestamp deadline;
         Integer point;
     }
+
+
 }
