b8b15ec9836d74c57bb4420a813c58232e1b8c69
Min Yeong Park <yingo24655@naver.com>
2024-02-06 11:11:57 +0900

    #80 Refactor: User 관련 기능 swagger 간단 설명 작성

--- src/main/java/friend/spring/web/controller/UserRestController.java
+++ src/main/java/friend/spring/web/controller/UserRestController.java
@@ -51,6 +51,7 @@ public class UserRestController {
     }
 
     @PostMapping ("/mailSend")//이메일 인증 코드 전송
+    @Operation(summary = "이메일 인증 코드 전송 API",description = "이메일 인증 코드 전송하는 API입니다.")
     public ApiResponse<UserResponseDTO.EmailSendRes> mailSend(@RequestBody @Valid UserRequestDTO.EmailSendReq emailDto){
         System.out.println("이메일 인증 요청이 들어옴");
         System.out.println("이메일 인증 이메일 :"+emailDto.getEmail());
@@ -59,6 +60,7 @@ public class UserRestController {
         return ApiResponse.onSuccess(UserConverter.toEmailSendRes(code));
     }
     @PostMapping("/mailauthCheck")//이메일 코드 확인
+    @Operation(summary = "이메일 코드 확인 API",description = "이메일 코드 확인하는 API입니다.")
     public ApiResponse<Void> mailauthCheck(@RequestBody @Valid UserRequestDTO.EmailSendCheckReq emailSendCheckReq){
 
     mailService.CheckAuthNum(emailSendCheckReq.getEmail(), emailSendCheckReq.getAuthNum());
@@ -88,6 +90,7 @@ public class UserRestController {
         return ApiResponse.onSuccess(UserConverter.toAnswerResDTO(myPage, nxtLevel, myCommentList));
     }
     @PostMapping("/join")//회원가입
+    @Operation(summary = "회원가입 API",description = "회원가입하는 API입니다.")
     public ApiResponse<UserResponseDTO.JoinResultDTO> join(@RequestBody @Valid UserRequestDTO.UserJoinRequest userJoinRequest) {
 
        User user = userService.joinUser(userJoinRequest);
@@ -97,6 +100,7 @@ public class UserRestController {
 
     //로그인
     @PostMapping("/login")
+    @Operation(summary = "로그인 API",description = "로그인하는 API입니다.")
     public ApiResponse<List<TokenDTO>> login(@RequestBody UserRequestDTO.UserLoginRequest userLoginRequest)throws GeneralException{
         List<TokenDTO> tokenDTOList = userService.login(userLoginRequest);
         return ApiResponse.onSuccess(tokenDTOList);
@@ -104,6 +108,7 @@ public class UserRestController {
 
     // 토큰 재발급
     @PostMapping("/reissue")
+    @Operation(summary = "토큰 재발급 API",description = "토큰 재발급하는 API입니다.")
     public ApiResponse<List<TokenDTO>> reissue(@RequestHeader(name = "rtk") String rtk, HttpServletRequest request) {
         System.out.println("controller: reissue 함수 실행");
         return ApiResponse.onSuccess(userService.reissue(request));
@@ -111,6 +116,7 @@ public class UserRestController {
 
     // 로그아웃
     @PostMapping("/logout")
+    @Operation(summary = "로그아웃 API",description = "로그아웃하는 API입니다.")
     public ApiResponse<String> logout(@RequestHeader(name = "atk") String atk, HttpServletRequest request)  {
         return ApiResponse.onSuccess(userService.logout(request));
     }
