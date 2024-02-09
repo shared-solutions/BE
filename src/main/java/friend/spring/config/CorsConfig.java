38a5805a25eca6fe55dec3564cb00373a9ae9632
Min Yeong Park <yingo24655@naver.com>
2024-02-09 22:52:25 +0900

    #29 Refactor: Cors 설정 변경

--- src/main/java/friend/spring/config/CorsConfig.java
+++ src/main/java/friend/spring/config/CorsConfig.java
@@ -11,6 +11,8 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
                 .allowedMethods("*")
                 .exposedHeaders("*");
     }
