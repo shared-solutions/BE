6fb9cedf2070dba2093ebd3d99058301b52a2430
hjh <ghkdwngns@naver.com>
2024-02-12 23:19:10 +0900

    글 상세보기, 글 투표하기시 나노초 단위로 마감시간 확인, 글 상세보기시 PostVoteType return 추가

--- src/main/java/friend/spring/apiPayload/code/status/ErrorStatus.java
+++ src/main/java/friend/spring/apiPayload/code/status/ErrorStatus.java
@@ -55,6 +55,7 @@ public enum ErrorStatus implements BaseErrorCode {
     NOT_ENOUGH_POINT(HttpStatus.BAD_REQUEST,"POST4013","해당 유저의 포인트가 부족 합니다"),
     POST_LIKE_DUPLICATE(HttpStatus.BAD_REQUEST, "POST4014", "글에 대한 좋아요 데이터가 이미 존재합니다."),
     POST_SCRAP_DUPLICATE(HttpStatus.BAD_REQUEST, "POST4015", "글에 대한 스크랩 데이터가 이미 존재합니다."),
+    DEADLINE_OVER(HttpStatus.BAD_REQUEST,"POST4016","투표 마감 시간이 지났습니다"),
 
     // 댓글 관련 응답
     COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "COMMENT4001", "댓글을 찾을 수 없습니다."),
