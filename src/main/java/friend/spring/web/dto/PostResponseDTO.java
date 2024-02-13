989d7ed92e55e92860013d307eb00265b8431004
hjh <ghkdwngns@naver.com>
2024-02-14 02:06:03 +0900

    #118 글 상세보기, 고민글 전체보기시 게이지투표 한정 PollTitle 추가

--- src/main/java/friend/spring/web/dto/PostResponseDTO.java
+++ src/main/java/friend/spring/web/dto/PostResponseDTO.java
@@ -77,6 +77,7 @@ public class PostResponseDTO {
         LocalDateTime uploadDate;
         List<PollOptionDTO.PollOptionRes> pollOption;
         List<PollOptionDTO.PollOptionRes> userVote;
+        String pollTitle; //게이지투표만 해당(게이지 투표 이름)
         Integer gauge;
         Integer like;
         Integer comment;
