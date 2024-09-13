e409b7a56540928e4be4e63381e63f6d63052d94
kimtaeyoung <62414583+kimtaeyoung201910794@users.noreply.github.com>
2024-09-13 15:11:25 +0900

    payment controller 수정

--- src/main/java/friend/spring/web/controller/PaymentController.java
+++ src/main/java/friend/spring/web/controller/PaymentController.java
@@ -10,6 +10,8 @@ import friend.spring.service.PaymentService;
 import friend.spring.web.dto.PaymentCallback;
 import friend.spring.web.dto.PaymentResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,8 @@ import org.springframework.web.bind.annotation.*;
 public class PaymentController {
 
     private final PaymentService paymentService;
+    private static final Logger log = LoggerFactory.getLogger(PaymentController.class);  // 로그 출력을 위한 선언입니다.
+
 
 
     @GetMapping("/payment/{orderId}")
@@ -29,16 +33,11 @@ public class PaymentController {
     }
 
     @PostMapping("/payment")
-    public IamportResponse<Payment> validationPayment(@RequestBody PaymentCallback paymentCallback) {
-        IamportResponse<Payment> iamportResponse =
+    public ApiResponse<IamportResponse<Payment>> validationPayment(@RequestBody PaymentCallback paymentCallback) {
+        IamportResponse<Payment> iamportResponse = paymentService.paymentByCallBack(paymentCallback);
+        log.info("결제 응답입니다.", iamportResponse.getResponse().toString()); // 결제 응답 로그 출력입니다.
+        return ApiResponse.onSuccess(iamportResponse);
     }
-
-
-
-
-
-
-
 
 
 }
